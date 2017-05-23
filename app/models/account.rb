99da23d365e3a8b8ba867a401cbf765241365383
Cesar Carruitero <cesar@mozilla.pe>
2017-05-23 12:21:06 -0500

    add transaction form

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,12 @@ class Account < ApplicationRecord
   validates :address, uniqueness: true
   has_many :transactions
 
+  TRANSACTION_TYPES = ['deposito', 'retiro']
+  LabelValue = Struct.new(:label, :value)
+  TRANSACTION_TYPES_SELECT = TRANSACTION_TYPES.collect do |transaction|
+    LabelValue.new(transaction, transaction)
+  end
+
   def to_param
     address
   end
