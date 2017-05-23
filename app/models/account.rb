0385f94c27c4792d102cfbccc238f321dbf54913
Cesar Carruitero <cesar@mozilla.pe>
2017-05-23 11:55:14 -0500

    cleanup account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,15 @@
 class Account < ApplicationRecord
+  validates :address, uniqueness: true
+
+  def to_param
+    address
+  end
+
+  def self.find(input)
+    find_by_address(input)
+  end
+
+  def sha_address(string)
+    Digest::SHA256.hexdigest(string)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :transactions
-  resources :accounts
+  resources :accounts, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
